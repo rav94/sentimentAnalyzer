cb09b2347693415b6150590b25be07f2e375df43
RavinduNF <fernandoravindu1994@gmail.com>
2020-03-05 23:17:13 +0530

    Add POST method request object validations and perprocessing logic

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import os.path
+import json
+import validators
 from flask import Flask, render_template, request, jsonify, make_response
 from SentimentAnalyzer import SentimentAnalyzer
 from PropertyReader import PropertyReader
@@ -11,19 +13,26 @@ api_context = env_props.get('api_context')
 
 @app.route(api_context)
 def home():
-    return make_response(jsonify({'error': 'Resource Not found'}), 404)
+    return make_response(jsonify({'error': 'Helath Check Up'}), 200)
 
 @app.route(api_context + '/analyze', methods = ['POST'])
 def analyze():
     if request.method == 'POST':
+        print(request.data);
         data = json.loads(request.data)
-        url = data.get("url", None)
-        if url is None:
-            return make_response(jsonify({'error': 'URL parameter not available. check your request'}), 400)
+        if "" in data:
+            return make_response(jsonify({'error': 'Your request is empty, please send a valid request'}), 400)
         else:
-            result = SentimentAnalyzer.sentiment_analyze_invoke(url)
-            return make_response(jsonify({'result': result}), 200)
-
+            url = data.get("url", None)
+            if url is None:
+                return make_response(jsonify({'error': 'URL parameter not available. check your request'}), 400)
+            else:
+                if not validators.url(url):
+                    return make_response(jsonify({'error': 'URL sent was not valid'}), 400)
+                else:
+                    result = SentimentAnalyzer.sentiment_analyze_invoke(url)
+                    return make_response(jsonify({'result': result}), 200)
+    
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'Resource Not found'}), 404)
